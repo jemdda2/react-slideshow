980222352b5aa8aa133e2fb090bb8767b91836be
Jisoon <siriusda2@naver.com>
2020-07-11 18:13:56 +0900

    Updates

---
+++ precache-manifest.6df7b1fb94e4a37cb4120437dac36615.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb51c0830b25b7d9f632d0efe27d5800",
+    "url": "/react-slideshow/index.html"
+  },
+  {
+    "revision": "99f5fb610df1c0ef3a37",
+    "url": "/react-slideshow/static/css/main.b4cd7faa.chunk.css"
+  },
+  {
+    "revision": "7769003ba256675dbed4",
+    "url": "/react-slideshow/static/js/2.92d4e877.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-slideshow/static/js/2.92d4e877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99f5fb610df1c0ef3a37",
+    "url": "/react-slideshow/static/js/main.c0db845b.chunk.js"
+  },
+  {
+    "revision": "8e0204a642311af23296",
+    "url": "/react-slideshow/static/js/runtime-main.27532d38.js"
+  }
+]);
